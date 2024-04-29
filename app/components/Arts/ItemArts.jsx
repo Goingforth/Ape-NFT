0b571a9d40d901e1053793dd3d096e4bcc2aade4
Oleksandr Siukov <as-service2008@ukr.net>
2024-04-29 21:42:58 +0300

    arts spinner

--- app/components/Arts/ItemArts.jsx
+++ app/components/Arts/ItemArts.jsx
@@ -1,33 +1,33 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import styles from "./styles.module.css";
-import { SpinnerDotted } from "spinners-react";
+
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 
-const ItemArts = (props) => {
+const ItemArts = ({ src }) => {
   const [loadedSrc, setLoadedSrc] = useState(null);
   useEffect(() => {
     setLoadedSrc(null);
-    if (props.src) {
+    if (src) {
       const handleLoad = () => {
-        setLoadedSrc(props.src);
+        setLoadedSrc(src);
       };
       const image = new Image();
       image.addEventListener("load", handleLoad);
-      image.src = props.src;
+      image.src = src;
       return () => {
         image.removeEventListener("load", handleLoad);
       };
     }
-  }, [props.src]);
-  if (loadedSrc === props.src) {
+  }, [src]);
+  if (loadedSrc === src) {
     return (
       <div className={styles.itemArts}>
         <LazyLoadImage
           className={styles.imgArts}
           alt='Picture of the NFT'
-          src={props.src}
+          src={src}
           effect='blur'
           wrapperProps={{
             // If you need to, you can tweak the effect transition using the wrapper style.
@@ -44,13 +44,11 @@ const ItemArts = (props) => {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: "var(--color-white_light)",
+        borderRadius: 12,
       }}
     >
-      <SpinnerDotted
-        size='50%'
-        style={{ margin: 0 }}
-        color='var(--color-white)'
-      />
+      LOADING.....
     </div>
   );
 };
